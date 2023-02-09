59e912ede9f71a382f3b4ee35c92ffa29f1f11f9
Fatma <fatmahayirli42@hotmail.com>
2023-02-08 19:45:44 -0500

    week3 TC001.java

--- src/test/java/com/cydeo/tests/week03/TC001.java
+++ src/test/java/com/cydeo/tests/week03/TC001.java
@@ -1,7 +1,32 @@
 package com.cydeo.tests.week03;
 
-public class TC001 {
+import com.cydeo.tests.tests.base.VytrackBase;
+import com.cydeo.utilities.BrowserUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class TC001 extends VytrackBase {
 
+//TC 1- As a user I should be able to see the labels [“Home”,”About us”,”Our Approach”,”Products and Services”,”Contact”,”LOGIN”]
+//1- Open a chrome browser
+//2- Go to "https://vytrack.com/"
+//3- Verify that ["Home","About us","Our Approach","Products and Services","Contact","LOGIN"] is displayed
+
+    @Test
+    public void verify_menu_labels(){
+
+        List<String> expectedLabels = new ArrayList<>(Arrays.asList("Home","About us","Our Approach","Products and Services","Contact","LOGIN"));
+
+        List<WebElement> elements = driver.findElements(By.xpath("//ul[@id='top-menu']/li"));
+        List<String> actualLabels = BrowserUtils.getTexts(elements);
+
+        Assert.assertEquals(actualLabels,expectedLabels);
+    }
 }
